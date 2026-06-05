41d00d79dda8def251998679c0ab27f26e3586d0
dangkhoi88x <36098048+dangkhoi88x@users.noreply.github.com>
2026-06-05 11:36:31 +0700

    Track Microservice-ecom as normal folder

---
+++ Microservice-ecom/src/main/java/com/example/microserviceecom/MicroserviceEcomApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserviceecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceEcomApplication.class, args);
+    }
+
+}
---
+++ Microservice-ecom/src/test/java/com/example/microserviceecom/MicroserviceEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microserviceecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
